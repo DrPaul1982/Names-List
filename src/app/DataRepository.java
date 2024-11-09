ad169424b642d283326293b87d69d88d9d273852
DrPaul1982 <avtoklavir@ukr.net>
2024-11-09 18:41:36 +0200

    Update DataRepository.java
    Fix DataRepository

--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataRepository {
-    public DataRepository() {
-    }
-
+  
     public List<String> getData() {
         List<String> list = new ArrayList<>();
         list.add("Alice");
